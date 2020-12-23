0e0f9b1a88f51036110d5ee16f8075338be352c9
a9200900 <firetruck9200900@gmail.com>
2020-12-24 00:23:27 +0800

    msg

--- app.py
+++ app.py
@@ -74,28 +74,28 @@ def callback():
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
         if isinstance(event, MessageEvent):
-            if machine.state == "intro":
-                if event.message.text == "進入":
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text ='Buttoms template',
-                            template = ButtomTemplate(
-                                title = '選項',
-                                text = '無盡天使:歡迎來到這個世界，你一定是上帝派來拯救我們的勇者，請你幫助我們打到大魔王『斯巴拉斯．魔迪耶爾』!',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label = '人物介紹',
-                                        text = '人物介紹'
-                                    ),
-                                    MessageTemplateAction(
-                                        label = '開始冒險',
-                                        text = '開始冒險'
-                                    )
-                                ]
-                            )
+           
+            if event.message.text == "進入":
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TemplateSendMessage(
+                        alt_text ='Buttoms template',
+                        template = ButtomTemplate(
+                            title = '選項',
+                            text = '無盡天使:歡迎來到這個世界，你一定是上帝派來拯救我們的勇者，請你幫助我們打到大魔王『斯巴拉斯．魔迪耶爾』!',
+                            actions=[
+                                MessageTemplateAction(
+                                    label = '人物介紹',
+                                    text = '人物介紹'
+                                ),
+                                MessageTemplateAction(
+                                    label = '開始冒險',
+                                    text = '開始冒險'
+                                )
+                            ]
                         )
                     )
+                )
             continue
         if not isinstance(event.message, TextMessage):
             continue
