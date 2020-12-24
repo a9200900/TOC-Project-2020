29726faa054347fd2231afb0f8980aa3afa8b3a9
a9200900 <firetruck9200900@gmail.com>
2020-12-24 08:53:15 +0800

    msg

--- fsm.py
+++ fsm.py
@@ -7,6 +7,15 @@ from linebot import LineBotApi, WebhookParser
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 line_bot_api = LineBotApi(channel_access_token)
+occupation = ""
+health = 0
+attack = 0
+defense = 0
+backpack = []
+equipment = []
+moster = []
+map = []
+
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -16,25 +25,25 @@ class TocMachine(GraphMachine):
     def on_enter_intro(self , event):
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "無盡天使:歡迎來到這個世界，你一定是上帝派來拯救我們的勇者，請你幫助我們打到大魔王『斯巴拉斯．魔迪耶爾』!")
-
-    def introduce(self , event):
-        reply_token = event.reply_token
-        send_text_message(reply_token, "現在是魔王曆128年,自從上一位勇者犧牲已經100多年了，沒有人能夠與現在的魔王抗衡，希望勇者您能幫助我們打到魔王!")
-
-    def orientation(self , event):
-        reply_token = event.reply_token
-        send_text_message(reply_token, "歡迎進入世界。\n輸入 人物介紹 可了解基本背景\n輸入 開始冒險 可開始偉大的旅程")
-        
+        send_text_message(reply_token, "無盡天使:歡迎來到這個世界，你一定是上帝派來拯救我們的勇者，請你幫助我們打到大魔王『斯巴拉斯．魔迪耶爾』!")   
 
     def on_enter_start(self , event):
+        global occupation = "狂戰士"
+        global health = 12
+        global attack = 3
+        global defense = 2
+        
         line_bot_api.reply_message(
                         event.reply_token,
                         TemplateSendMessage(
                             alt_text ='Buttons template',
                             template = ButtonsTemplate(
                                 title = '選項',
-                                text = '踏上旅程，在前方是未知的道路!',
+                                text = '踏上旅程，在前方是未知的道路!\n'+
+                                        '職業:'+occupation+'\n'+
+                                        '生命:'+health+'\n'+
+                                        '攻擊力:'+attack+'\n'+
+                                        '防禦力:'+defense+'\n',
                                 actions=[
                                     MessageTemplateAction(
                                         label = '戰鬥',
@@ -84,9 +93,9 @@ class TocMachine(GraphMachine):
                         )
                     )
 
-    def on_exit_state_fight(self , event):
-        reply_token = event.reply_token
-        send_text_message(reply_token, "戰鬥結束")
+    # def on_exit_state_fight(self , event):
+    #     reply_token = event.reply_token
+    #     send_text_message(reply_token, "戰鬥結束")
 
     def line_buttons_intro(self,event):
         line_bot_api.reply_message(
