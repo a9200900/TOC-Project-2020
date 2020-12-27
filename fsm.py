9a63fd49bdfe567820829b3c1a4eb99114e1ddcb
a9200900 <firetruck9200900@gmail.com>
2020-12-27 17:44:30 +0800

    msg

--- fsm.py
+++ fsm.py
@@ -1240,12 +1240,9 @@ class TocMachine(GraphMachine):
         if occupation =="狂戰士":
             tmp = random.randint(1,10)
             if tmp <= 5:
-                if monster_now_count>=0:
-                    if monster_now_count <6:
-                        drop_tmp = random.choice(drop_1_sword)
-                        backpack.append(drop_tmp)
-                        backpack.append("炎王龍雙刀")
-                        tmp_text ="\n怪物掉落了 "+drop_tmp
+                backpack.append(random.choice(drop_1_sword))
+                backpack.append("炎王龍雙刀")
+                tmp_text ="\n怪物掉落了 "+drop_tmp
                 if monster_now_count>=6:
                     if monster_now_count <12:
                         drop_tmp = random.choices(drop_2_sword)
